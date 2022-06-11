b95491d343cc283ccb3bfbae832b0ac88997b942
Shambuwu <Balfappius@protonmail.ch>
2022-06-12 00:03:13 +0200

    Add setup for memory game

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let randomLetters = [];
 let numberOfCards = 36;
 
-
-for (let i = 0; randomLetters.length < numberOfCards/2; i++){
+for (let i = 0; randomLetters.length < numberOfCards / 2; i++) {
     let x = alphabet[Math.floor(Math.random() * alphabet.length)];
     let temp = true;
-    randomLetters.forEach(value => { if (x === value) temp = false; });
-    if(temp){
+    randomLetters.forEach(value => {
+        if (x === value) temp = false;
+    });
+    if (temp) {
         randomLetters.push(x);
     }
 }
@@ -15,17 +16,44 @@ randomLetters = randomLetters.concat(randomLetters);
 console.log(randomLetters);
 
 const cards = document.querySelectorAll(".memory-card");
-const backFaces = document.querySelectorAll(".back-face")
+const backFaces = document.querySelectorAll(".back-face");
 
 backFaces.forEach((face, index) => {
-    face.innerHTML=randomLetters[index];
+    face.innerHTML = randomLetters[index];
 })
 
-cards.forEach(function (card) {
-    card.addEventListener('click', function () {
-        cards.forEach(function (c) {
-            if (c !== card) c.classList.remove('flip');
-            else c.classList.toggle('flip');
-        });
-    });
-});
+let lockBoard = false;
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    if (lockBoard) return;
+    this.classList.add("flip");
+
+    if (!hasFlippedCard) {
+        this.removeEventListener("click", flipCard);
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    } else {
+        secondCard = this;
+        hasFlippedCard = false;
+    }
+
+    if (firstCard.innerText[2] === secondCard.innerText[2]) {
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEventListener("click", flipCard);
+    } else {
+        lockBoard = true;
+        setTimeout(() => {
+            firstCard.addEventListener("click", flipCard);
+            firstCard.classList.remove("flip");
+            secondCard.classList.remove("flip");
+            lockBoard = false;
+        }, 1500);
+    }
+}
+
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+})
