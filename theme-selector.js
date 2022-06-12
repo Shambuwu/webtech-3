3085be288c1b325d1c975d690e4f80ed18710c1b
Shambuwu <Balfappius@protonmail.ch>
2022-06-12 12:41:45 +0200

    Add none image type and tidy up color selector functionality

--- theme-selector.js
+++ theme-selector.js
@@ -1,22 +1,10 @@
 // Color picker
 
-const cardColorPicker = document.getElementById("color-picker-card");
-const openCardColorpicker = document.getElementById("color-picker-card-open");
-const foundCardColorPicker = document.getElementById("color-picker-card-found");
-const colorPickers = [cardColorPicker, openCardColorpicker, foundCardColorPicker];
+const selectorTypes = {"color-picker-card": "--primary-card-color", "color-picker-card-open": "--primary-open-color", "color-picker-card-found": "--primary-found-color"}
+const colorPickers = document.querySelectorAll(".color-picker input")
 
 function changeColor() {
-    switch(this.id){
-        case "color-picker-card":
-            document.documentElement.style.setProperty("--primary-card-color", this.value);
-            break;
-        case "color-picker-card-open":
-            document.documentElement.style.setProperty("--primary-open-color", this.value);
-            break;
-        case "color-picker-card-found":
-            document.documentElement.style.setProperty("--primary-found-color", this.value);
-            break;
-    }
+    document.documentElement.style.setProperty(selectorTypes[this.id.toString()], this.value);
 }
 
 colorPickers.forEach((cp) => {
@@ -25,23 +13,20 @@ colorPickers.forEach((cp) => {
 
 // Image Selector
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-const selector = document.getElementById("image-selector");
+const imageSelector = document.getElementById("image-selector");
 const frontFaces = document.querySelectorAll(".front-face");
-const imageUrls = {Random: "https://picsum.photos/200", Dogs: "https://dog.ceo/api/breeds/image/random", Cats: "https://placekitten.com/640/360"};
-
-let value = selector.options[selector.selectedIndex].value;
-selector.addEventListener("change", () => {
-    value = selector.options[selector.selectedIndex].value;
-    changeBackground(value);
-})
+const imageUrls = {None: null, Random: "https://picsum.photos/200", Dogs: "https://dog.ceo/api/breeds/image/random", Cats: "https://placekitten.com/640/360"};
 
 function changeBackground(imageType) {
+    if (imageUrls[imageType] === null) {
+        frontFaces.forEach((face) => {
+            face.style.backgroundImage = null;
+        })
+        return;
+    }
+
     frontFaces.forEach((face) => {
-        fetch(imageUrls[value])
+        fetch(imageUrls[imageType])
             .then((response) => response.blob())
             .then((imageBlob) => {
                 let imageUrl = URL.createObjectURL(imageBlob);
@@ -50,7 +35,12 @@ function changeBackground(imageType) {
     })
 }
 
-changeBackground(imageUrls.Random)
+let value;
+
+imageSelector.addEventListener("change", () => {
+    value = imageSelector.options[imageSelector.selectedIndex].value;
+    changeBackground(value);
+})
 
 
 
